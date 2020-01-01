02ee432ef9cbd0a167c1dba3eb8ecfa17e7cd680
K3be <ahmed.13.alkaaby@gmail.com>
2020-01-01 15:41:21 +0000

    Automatically backed up by Learn

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -34,7 +34,7 @@ export default class ChromeBoisDomain extends Component {
       <canvas
         onMouseMove={this.handleMouseMove}
         onClick={this.handleClick}
-        onPress={this.onPress}
+        onKeyPress={this.onPress}
         width='900'
         height='600'
         tabIndex="0">
